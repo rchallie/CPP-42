4d5af46b9164201ebd3859d4bb81f0c296a3295a
rchallie <unconfigured@null.spigotmc.org>
2020-10-03 00:10:23 +0200

    Add main to Module-01/ex01

--- Module-cpp-01/ex01/ex01.cpp
+++ Module-cpp-01/ex01/ex01.cpp
@@ -3,13 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ex01.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: excalibur <excalibur@student.42.fr>        +#+  +:+       +#+        */
+/*   By: rchallie <rchallie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/14 16:51:34 by excalibur         #+#    #+#             */
-/*   Updated: 2020/03/14 16:58:24 by excalibur        ###   ########.fr       */
+/*   Created: 2020/03/14 16:51:34 by rchallie          #+#    #+#             */
+/*   Updated: 2020/10/03 00:08:25 by rchallie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 
 void    memoryLeak()
 {
@@ -18,3 +19,14 @@ void    memoryLeak()
     std::cout << *panthere << std::endl;
     delete panthere;
 }
+
+/*
+** To check if no leaks use:
+** Remove comments to main
+** valgrind --leak-check=full ./a.out
+*/
+
+// int main()
+// {
+//     memoryLeak();
+// }
